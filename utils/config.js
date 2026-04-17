ff93ba12d5a192e6d62883b099859ca9dd8f447c
hrtxyz123-droid <hrtxyz123@gmail.com>
2026-04-17 11:26:14 +0530

    Update config.js

--- utils/config.js
+++ utils/config.js
@@ -1,12 +1,5 @@
 export const CONFIG = {
-    // The address of your local Node.js Proxy Server
-    PROXY_URL: "http://localhost:3000/fetch-emails",
+    PROXY_URL: "http://localhost:3000",
     
-    // Google Safe Browsing API Key
-    SAFE_BROWSING_API_KEY: "Your_api_key",
-    SAFE_BROWSING_URL: "https://safebrowsing.googleapis.com/v4/threatMatches:find",
-    
-    // Security Thresholds
-    AI_CONFIDENCE_THRESHOLD: 0.75, // Only alert if AI is 75% sure
-    MAX_TEXT_LENGTH: 500           // Matches your ML model's input size
+    AI_CONFIDENCE_THRESHOLD: 0.7
 };
